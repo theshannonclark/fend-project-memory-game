4124a65c65e349afe8fa48ecf29deafeae5ea386
Shannon Clark <theshannonclark@gmail.com>
2017-12-06 10:41:42 +1100

    Fix javascript loading bug
    Was using jQuery.getScript to load script files, but
    apparently google chrome considers loading locally
    hosted files to be a cross-origin request, and refused
    to load them.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
 /**
- * app.js -- Main application script.
+ * app.js -- Main application script
  */
 
-/* 
-  In a real application, it would probably be better to
-  compile/minify all of these into one file, but I hope 
-  doing it this way is OK for this project.
-*/
-
-// Getting weird XML parsing errors, even though
-// they're script files...
-
-$.getScript('js/model/Board.js');
-$.getScript('js/model/Card.js');
-$.getScript('js/view/BoardView.js');
-$.getScript('js/view/ScorePanelView.js');
-$.getScript('js/view/WinModalView.js');
-$.getScript('js/utils.js');
-$.getScript('js/Timer.js');
-$.getScript('js/GameController.js')
-  .done(function() {
-    let gameController = new GameController();
-  });
+// Run the application
+let gameController = new GameController();
